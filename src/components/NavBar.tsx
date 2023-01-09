155cdb1c17121a1edc2796d267a94ccd03761b75
jessica-guedes <jessicaguedes.ads@gmail.com>
2023-01-09 16:47:09 -0400

    add menu close and open

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,9 +2,10 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import Logo from "../assets/logo.png"
-
+import { VscMenu, VscChromeClose } from "react-icons/vsc";
 export default function NavBar(){
   let [open, setOpen] = useState(false);
+
 
   return(
     <div>
@@ -20,30 +21,12 @@ export default function NavBar(){
           </a>
 
 
-          <button 
-          onClick={()=> setOpen(!open)} 
-          
-          data-collapse-toggle="navbar-default" 
-          type="button" 
-          className="inline-flex items-center p-2 ml-3 text-sm rounded-lg md:hidden text-gray-400" 
-          aria-controls="navbar-default" 
-          aria-expanded="false">
-            
-          <span className="sr-only">
-            Open main menu
-          </span>
+          <div onClick={()=> setOpen(!open)} className="text-2xl text-white">
+          {open ? <VscChromeClose/> : <VscMenu /> }
+          </div>
 
-          <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path>
-          </svg>
-
-          </button>
-
-          <div className={`w-full md:block md:w-auto ${open ? 'text-center mb-[500px]' : 'hidden'}`} >
-            <ul className="flex flex-col p-4 
-            
-            md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-gray-900"
-            
-            >
+          <div className={`w-full md:block md:w-auto ${open ? 'text-center mb-[500px]' : 'hidden' }`} >
+            <ul className="flex flex-col p-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-gray-900">
               <li>
             <Link href="/" className="block py-8 pl-3 pr-4 text-gray-400 md:p-0 md:hover:text-white md:hover:bg-transparent sm:hover:text-white sm:hover:bg-transparent">Home</Link>
               </li>
