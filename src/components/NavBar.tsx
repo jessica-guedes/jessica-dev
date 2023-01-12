7802dc7835bf2ecfb7791066eb0daa002edbf8cd
jessica-guedes <jessicaguedes.ads@gmail.com>
2023-01-12 15:43:32 -0400

    added behavior smooth

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import { Link } from 'react-scroll';
 import { useState } from "react";
 import Logo from "../assets/logo.png"
 import { VscMenu, VscChromeClose } from "react-icons/vsc";
 export default function NavBar(){
   let [open, setOpen] = useState(false);
 
+  
 
   return(
     <div>
@@ -26,18 +28,19 @@ export default function NavBar(){
 
           <div className={`w-full md:block md:w-auto ${open ? 'text-center mb-[500px]' : 'hidden' }`} >
             
-            <ul className="flex flex-col p-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-gray-900">
+            <ul className="flex flex-col p-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-gray-900 cursor-pointer">
               <li>
-            <a href="./" className="block py-8 pl-3 pr-4 text-gray-400 md:p-0 md:hover:text-white md:hover:bg-transparent sm:hover:text-white sm:hover:bg-transparent" onClick={() => setOpen(false)}>Home</a>
+            <Link to="/" smooth={true} className="block py-8 pl-3 pr-4 text-gray-400 md:p-0 md:hover:text-white md:hover:bg-transparent sm:hover:text-white sm:hover:bg-transparent" onClick={() => setOpen(false)}>Home</Link>
               </li>
               <li>
-                <a href="#stacks" className="block py-8 pl-3 pr-4 md:border-0 md:p-0 text-gray-400 md:hover:text-white md:hover:bg-transparent" onClick={() => setOpen(false)}>Stacks</a>
+                <Link to="stacks" smooth={true} spy={true} offset={-70} duration={500} 
+                className="block py-8 pl-3 pr-4 md:border-0 md:p-0 text-gray-400 md:hover:text-white md:hover:bg-transparent" onClick={() => setOpen(false)}>Stacks</Link>
               </li>
               <li>
-                <a href="#projects" className="block py-8 pl-3 pr-4 md:border-0 md:p-0 text-gray-400 md:hover:text-white md:hover:bg-transparent" onClick={() => setOpen(false)}>Projetos</a>
+                <Link to="projects" smooth={true} spy={true} offset={-70} duration={500} className="block py-8 pl-3 pr-4 md:border-0 md:p-0 text-gray-400 md:hover:text-white md:hover:bg-transparent" onClick={() => setOpen(false)}>Projetos</Link>
               </li>
               <li>
-                <a href="#contact" className="block py-8 pl-3 pr-4 md:border-0 md:p-0 text-gray-400 md:hover:text-white md:hover:bg-transparent " onClick={() => setOpen(false)}>Contato</a>
+                <Link to="contact" smooth={true} spy={true} offset={-70} duration={500} className="block py-8 pl-3 pr-4 md:border-0 md:p-0 text-gray-400 md:hover:text-white md:hover:bg-transparent " onClick={() => setOpen(false)}>Contato</Link>
               </li>
             </ul>
           </div>
